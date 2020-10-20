c5c314c674b48d4210907d0ee7f31734b84dc2fe
Maxim Thomas <maxim.thomas.be@gmail.com>
2020-10-20 17:42:06 +0200

    refactor - extract isMultipleOf5

--- FizzBuzz.php
+++ FizzBuzz.php
@@ -5,7 +5,7 @@ class FizzBuzz{
     public function count(int $int): string
     {
         if($this->isMultipleOf3($int)) return "Fizz";
-        if($int % 5 === 0) return "Buzz";
+        if($this->isMultipleOf5($int)) return "Buzz";
 
         return strval($int);
     }
@@ -13,5 +13,10 @@ class FizzBuzz{
     public function isMultipleOf3(int $int): bool
     {
         return $int % 3 === 0;
+    }
+
+    public function isMultipleOf5(int $int): bool
+    {
+        return $int % 5 === 0;
     }
 }
